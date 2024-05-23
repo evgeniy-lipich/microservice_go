9d392f912ca1103d0f36bc47151cccc8c14ef179
evgeniy lipich <evgeniylipich44.92@gmail.com>
2024-05-23 10:03:57 +0300

    update http client, update grpc client

--- week_1/grpc/cmd/grpc_client/main.go
+++ week_1/grpc/cmd/grpc_client/main.go
@@ -18,21 +18,29 @@ const (
 )
 
 func main() {
+	// установить соединение
 	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect to server: %v", err)
 	}
-	defer conn.Close()
+	defer func() {
+		err = conn.Close()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}()
 
-	c := desc.NewNoteV1Client(conn)
-
+	// в контексте задаем ограничение по таймауту
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	r, err := c.Get(ctx, &desc.GetRequest{Id: noteId})
+	// создать клиент Note, передаем соединение
+	client := desc.NewNoteV1Client(conn)
+
+	// get запрос
+	getResponse, err := client.Get(ctx, &desc.GetRequest{Id: noteId})
 	if err != nil {
 		log.Fatalf("failed to get note by id: %v", err)
 	}
-
-	log.Printf(color.RedString("Note info: \n"), color.GreenString("%+v", r.GetNote()))
+	log.Printf(color.RedString("Note info: \n"), color.GreenString("%+v", getResponse.GetNote()))
 }
